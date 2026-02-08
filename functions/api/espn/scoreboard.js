ca77b0927a7d64191e4537748c51a5b5902c9d18
mhuggins102-sudo <mhuggins102@gmail.com>
2026-02-07 19:23:51 -0500

    Create scoreboard.js

---
+++ functions/api/espn/scoreboard.js
@@ -0,0 +1,17 @@
+export async function onRequest({ request }) {
+  const url = new URL(request.url);
+  const target = new URL("https://site.api.espn.com/apis/site/v2/sports/football/nfl/scoreboard");
+  target.search = url.search;
+
+  const resp = await fetch(target.toString(), {
+    headers: { "Accept": "application/json" },
+  });
+
+  return new Response(resp.body, {
+    status: resp.status,
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+      "Cache-Control": "public, max-age=60",
+    },
+  });
+}
